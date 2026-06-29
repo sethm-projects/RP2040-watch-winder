488073ae4c096676e96b3cd1134b6679b27b9af7
Ben Merritt <ben@haradion.net>
2026-06-28 18:48:33 -0700

    Add switch class

---
+++ switch.py
@@ -0,0 +1,54 @@
+from machine import Pin
+from micropython import const
+from time import ticks_ms
+import uasyncio as asyncio
+
+from debounce import Debouncer
+
+_trigger = const(Pin.IRQ_RISING | Pin.IRQ_FALLING)
+
+class Switch:
+    """A debounced switch tied to a GPIO pin"""
+    _pin: Pin
+    _raw_state: int
+    _debounce: Debouncer
+    _flag: asyncio.ThreadSafeFlag
+    _task: asyncio.Task
+
+    def __init__(self, pin: Pin, debounce_interval_ms: int):
+        self._pin = pin
+        self._raw_state = pin.value()
+        self._flag = asyncio.ThreadSafeFlag()
+        pin.irq(trigger = _trigger, handler = self._irq, hard = True)
+        self._debounce = Debouncer(self._raw_state, debounce_interval_ms)
+
+    def stop(self):
+        self._task.cancel()
+        self._debounce.stop()
+        self._pin.irq(trigger = 0, handler = None)
+
+    def state(self):
+        """Returns the current input state"""
+        return self._current_state
+
+    async def wait_for_state_change(): void
+        """Waits for the debounced state to change"""
+        if self._stopped:
+            throw asyncio.CancelledError()
+        await self._flag.wait()
+        if self._stopped:
+            throw asyncio.CancelledError()
+
+    def _irq(self, pin):
+        new_state = pin.read()
+        if self._raw_state != new_state:
+            self._raw_state = new_state
+            self._flag.set()
+
+    async def _watch(self):
+        while True:
+            try:
+                await self._flag.wait()
+            except asyncio.CancelledError:
+                return
+            self._debounce.record_state(self._raw_state)
